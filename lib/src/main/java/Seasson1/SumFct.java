585ee52c1a62a16d217f985e06c228bbef90bd0c
Emilio Caballero <milocaballerob04@gmail.com>
2021-08-02 17:51:05 -0500

    Fix: Solution and tests for Stop gninnipS My sdroW! Kata
    Use index at a best way

--- lib/src/main/java/Seasson1/SumFct.java
+++ lib/src/main/java/Seasson1/SumFct.java
@@ -7,23 +7,18 @@ public class SumFct {
 
   public static BigInteger perimeter(BigInteger n) {
 
-    return Arrays.asList(fib(n)).stream().reduce(BigInteger.ZERO,BigInteger::add).multiply(BigInteger.valueOf(4));
+    return Arrays.stream(fib(n))
+        .reduce(BigInteger.ZERO, BigInteger::add)
+        .multiply(BigInteger.valueOf(4));
   }
 
-  private static BigInteger[] fib(final BigInteger n) {
-    Long size = n.longValue();
-    BigInteger [] cache = new BigInteger [size.intValue() + 1];
-
+  public static BigInteger[] fib(BigInteger n) {
+    BigInteger [] cache = new BigInteger[n.add(BigInteger.ONE).intValue()];
     cache[0] = BigInteger.ONE;
     cache[1] = BigInteger.ONE;
-
-    for(int i = 2; i <= size.intValue(); i++){
-      BigInteger fibValOne = cache[i-1];
-      BigInteger fibValTwo = cache[i-2];
-      cache[i] = fibValOne.add(fibValTwo);
+    for(BigInteger i = BigInteger.TWO; i.compareTo(n.add(BigInteger.ONE)) < 0; i = i.add(BigInteger.ONE)){
+      cache[i.intValue()] = cache[i.intValue() - 1].add(cache[i.intValue() - 2]);
     }
-
     return cache;
   }
 }
-
